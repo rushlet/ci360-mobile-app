7685d752b792a157d50ae99921fdd37c8bccacd6
Becky Rush <becky.rush14@yahoo.co.uk>
2018-02-28 13:21:38 +0000

    get urls for images from flickr

--- app/src/main/java/uk/ac/brighton/rlr17uni/inspirgram/InspirationFragment.java
+++ app/src/main/java/uk/ac/brighton/rlr17uni/inspirgram/InspirationFragment.java
@@ -7,6 +7,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Array;
 
 
 /**
@@ -45,7 +59,50 @@ public class InspirationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inspiration, container, false);
+        View rootview =  inflater.inflate(R.layout.fragment_inspiration, container, false);
+        String tag = "shadow";
+        String baseURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
+        String key =  "60dd9c77c36b4684185bb29b88d99c4c";
+        String url = baseURL + "&api_key=" + key +"&tags=" + tag +"&per_page=10&format=json&nojsoncallback=1";
+        final TextView mTextView = (TextView) rootview.findViewById(R.id.text);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        JSONObject obj = null;
+                        try {
+                            obj = new JSONObject(response);
+                            JSONObject photos = obj.getJSONObject("photos");
+                            JSONArray photoArray = photos.getJSONArray("photo");
+                            String[] photoURLs = new String[10];
+                            String urls = "";
+                            for(int i = 0; i < photoArray.length(); i++) {
+                                JSONObject photo = photoArray.getJSONObject(i);
+                                String url = "https://farm" + photo.getString("farm") + ".staticflickr.com/" + photo.getString("server") + "/"+photo.getString("id") + "_" + photo.getString("secret") + ".jpg";
+                                photoURLs[i] = url;
+                                urls = urls + url + ", ";
+                            }
+                            mTextView.setText("Response is: "+ urls);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            mTextView.setText("Response is: "+ response);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+        return rootview;
     }
 
 
